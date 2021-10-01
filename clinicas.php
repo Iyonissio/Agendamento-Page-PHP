493541c7c7f0f152fcf93c18aa39bebc6c8d33c2
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-10-01 16:13:53 +0200

    destaque

--- clinicas.php
+++ clinicas.php
@@ -27,10 +27,6 @@
 		width: 100%;
 	}
 
-	.slide-container .slide.fade {
-		animation: fade 0.5s cubic-bezier(0.55, 0.085, 0.68, 0.53) both;
-	}
-
 	.slide-container .slide img {
 		width: 100%;
 		height: 100vh;
@@ -60,170 +56,67 @@
 	}
 
 	.slide-container .antes {
-		left: 2px;
+		left: 1px;
 	}
 
 	.slide-container .proximo {
-		right: 2px;
-	}
-
-	.content {
-		padding-left: 0px;
-		padding-right: 0px;
+		right: 1px;
 	}
 
 	.ver-empresa {
 		cursor: pointer;
-		position: absolute;
-		top: 70%;
-		left: 45%;
 		color: white;
 		font-size: 16px;
 		text-decoration: none;
 		background-color: #F1A300;
-		padding: 8px 20px;
-		border-radius: 5px;
+		padding: 2px 2px;
+		border-radius: 2px;
+		padding-top: 10px;
 	}
 
 	.info-destaque-nome {
 		cursor: pointer;
-		position: absolute;
-		top: 55%;
-		left: 46%;
-		color: white;
+		color: black;
+		font-size: 28px;
 	}
 
 	.info-destaque-endereco {
 		cursor: pointer;
+		color: black;
+		font-size: 18px;
+	}
+
+
+	/* Parte do Hover */
+
+
+	.image {
+		opacity: 1;
+		display: block;
+		object-fit: cover;
+		width: 100%;
+		height: 100vh;
+		transition: .5s ease;
+		backface-visibility: hidden;
+	}
+
+	.middle {
+		transition: .5s ease;
+		opacity: 0;
 		position: absolute;
-		top: 60%;
-		left: 40%;
-		color: white;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		-ms-transform: translate(-50%, -50%);
+		text-align: center;
 	}
 
-	.em-destaque {
-		cursor: pointer;
-		position: absolute;
-		top: 20%;
-		left: 33%;
-		color: white;
+	.slide-container:hover .image {
+		opacity: 0.3;
 	}
 
-	@media (min-width: 100px) {
-		.ver-empresa {
-			cursor: pointer;
-			position: absolute;
-			top: 70%;
-			left: 25%;
-			color: white;
-			font-size: 16px;
-			text-decoration: none;
-			background-color: #F1A300;
-			padding: 8px 20px;
-			border-radius: 5px;
-		}
-
-		.info-destaque-nome {
-			cursor: pointer;
-			position: absolute;
-			top: 45%;
-			left: 30%;
-			color: white;
-		}
-
-		.info-destaque-endereco {
-			cursor: pointer;
-			position: absolute;
-			top: 55%;
-			left: 9%;
-			color: white;
-		}
-
-		.em-destaque {
-			cursor: pointer;
-			position: absolute;
-			top: 20%;
-			left: 33%;
-			color: white;
-		}
-	}
-
-	@media (min-width: 460px) and (max-width: 899px) {
-		.ver-empresa {
-			cursor: pointer;
-			position: absolute;
-			top: 70%;
-			left: 37%;
-			color: white;
-			font-size: 16px;
-			text-decoration: none;
-			background-color: #F1A300;
-			padding: 8px 20px;
-			border-radius: 5px;
-		}
-
-		.info-destaque-nome {
-			cursor: pointer;
-			position: absolute;
-			top: 55%;
-			left: 40%;
-			color: white;
-		}
-
-		.info-destaque-endereco {
-			cursor: pointer;
-			position: absolute;
-			top: 60%;
-			left: 30%;
-			color: white;
-		}
-
-		.em-destaque {
-			cursor: pointer;
-			position: absolute;
-			top: 20%;
-			left: 33%;
-			color: white;
-		}
-	}
-
-	@media (min-width: 900px) {
-		.ver-empresa {
-			cursor: pointer;
-			position: absolute;
-			top: 70%;
-			left: 45%;
-			color: white;
-			font-size: 16px;
-			text-decoration: none;
-			background-color: #F1A300;
-			padding: 8px 20px;
-			border-radius: 5px;
-		}
-
-		.info-destaque-nome {
-			cursor: pointer;
-			position: absolute;
-			top: 50%;
-			left: 46%;
-			color: white;
-		}
-
-		.info-destaque-endereco {
-			cursor: pointer;
-			position: absolute;
-			top: 60%;
-			left: 40%;
-			color: white;
-		}
-
-		.em-destaque {
-			cursor: pointer;
-			position: absolute;
-			top: 20%;
-			left: 33%;
-			color: white;
-		}
+	.slide-container:hover .middle {
+		opacity: 1;
 	}
 </style>
 
@@ -233,29 +126,34 @@
 		<!--Imagem  Estabelecimento 1 -->
 		<div class="slide fade">
 			<!-- Coloquei a imagem dentro de um link, assim ao clicar ira a decricao do Estabelecimento -->
-			<a href="#"><img src="image/salao1.jpg" alt=''></a>
-			<div class="info-destaque-nome">
-				<h3>Nome do Salao</h3>
-			</div>
-			<div class="info-destaque-endereco">
-				<p><strong>Endereco:</strong> 930 Av. Julius Nyerere, Maputo</p>
-			</div>
-			<div class="ver-empresa">
-				<h4>Ver Estabelecimento</h4>
+			<img src="image/Salao2.jpg" alt="Avatar" class="image">
+			<div class="middle">
+				<div class="info-destaque-nome">
+					<h3>Nome do Salao 1</h3>
+				</div>
+				<div class="info-destaque-endereco">
+					<p><strong>Endereco:</strong> 930 Av. Julius Nyerere, Maputo</p>
+				</div>
+				<div class="ver-empresa">
+					<h4>Ver Estabelecimento</h4>
+				</div>
 			</div>
 		</div>
-		<!-- Imagem Estabelecimento 2 -->
+
+		<!--Imagem  Estabelecimento 2 -->
 		<div class="slide fade">
 			<!-- Coloquei a imagem dentro de um link, assim ao clicar ira a decricao do Estabelecimento -->
-			<a href="#"></a><img src="image/Salao2.jpg" alt=''>
-			<div class="info-destaque-nome">
-				<h3>Nome do Salao</h3>
-			</div>
-			<div class="info-destaque-endereco">
-				<p><strong>Endereco:</strong> 930 Av. Julius Nyerere, Maputo</p>
-			</div>
-			<div class="ver-empresa">
-				<h4>Ver Estabelecimento</h4>
+			<img src="image/salao1.jpg" alt="Avatar" class="image">
+			<div class="middle">
+				<div class="info-destaque-nome">
+					<h3>Nome do Salao 2</h3>
+				</div>
+				<div class="info-destaque-endereco">
+					<p><strong>Endereco:</strong> 930 Av. Julius Nyerere, Maputo</p>
+				</div>
+				<div class="ver-empresa">
+					<h4>Ver Estabelecimento</h4>
+				</div>
 			</div>
 		</div>
 		<!--<h1 class="em-destaque">Estabelecimentos em Destaque</h1> -->
